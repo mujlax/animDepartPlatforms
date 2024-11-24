784ac5556d37e1a63aad7943c84ee659893dddcf
mujlax <42385663+mujlax@users.noreply.github.com>
2024-11-24 19:29:51 +0300

    yeah

---
+++ platforms/test.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'test',
+    process: async (paths, options, userLink, browserWindow) => {
+        console.log(`Обработка для платформы YandexRTB: ${paths}`);
+        for (const folderPath of paths) {
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            // Логика обработки
+        }
+    }
+};
