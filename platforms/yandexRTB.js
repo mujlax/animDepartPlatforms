0e77ca6502cee90625ffdec4e7ed4a0a0e30b025
mujlax <42385663+mujlax@users.noreply.github.com>
2024-11-27 10:29:53 +0300

    Add files via upload

---
+++ platforms/yandexRTB.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+const { ipcMain } = require('electron');
+const { minifyJSFiles, 
+    compressImages, 
+    replaceImagesWithBase64, 
+    inlineJavaScript, 
+    copyFolderSync, 
+    archiveFolder, 
+    deleteFiles, 
+    insertScriptAfterMarker, 
+    wrapDiv,
+    prepareReleaseFolder,
+    checkRequestLink
+} = require('../bannerUtils');
+
+module.exports = {
+    name: 'YandexRTB',
+    process: async (paths, userLink, platformWindow) => {
+        userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
+
+        for (const folderPath of paths) {
+            const releasePath = await prepareReleaseFolder(folderPath);
+            const sizeMatch = releasePath.match(/(\d+)x(\d+)/);
+            const [_, width, height] = sizeMatch || [null, '0', '0'];
+
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            console.log(`Папка скопирована в ${releasePath}`);
+            console.log(`Используемая ссылка: ${userLink}`);
+
+            await insertScriptAfterMarker(releasePath,
+                '<meta charset="UTF-8">',
+                `<meta name="ad.size" content="width=${width},height=${height}">`
+            );
+
+            await insertScriptAfterMarker(releasePath,
+                '</body>',
+                `<script>document.getElementById("click_area").href = yandexHTML5BannerApi.getClickURLNum(1);</script>\n</body>`,
+                true
+            );
+
+            try {
+                await wrapDiv(releasePath, 'animation_container', `<a id="click_area" href="#" target="_blank">`);
+                console.log('Div успешно обёрнут.');
+            } catch (error) {
+                console.error('Ошибка:', error.message);
+            }
+
+            await compressImages(releasePath);
+            //await replaceImagesWithBase64(releasePath);
+            await minifyJSFiles(releasePath);
+            //inlineJavaScript(releasePath);
+            await deleteFiles(releasePath, ['*.fla']);
+            await archiveFolder(releasePath);
+        }
+    }
+};
