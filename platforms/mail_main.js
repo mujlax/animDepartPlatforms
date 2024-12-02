d12f0bdd0a706bcb6c21bc33cb0071ae24a1b508
mujlax <42385663+mujlax@users.noreply.github.com>
2024-12-02 16:17:39 +0300

    Update mail_main.js

--- platforms/mail_main.js
+++ platforms/mail_main.js
@@ -16,7 +16,7 @@ const {
 } = require('../bannerUtils');
 
 module.exports = {
-    name: 'mail_Main_gifs2',
+    name: 'mail_Main',
     process: async (paths, userLink, platformWindow, gifSettings) => {
         userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
 
