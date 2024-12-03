4acdbfc0115589c7a30fd9630cfae422987fc6f3
mujlax <42385663+mujlax@users.noreply.github.com>
2024-12-03 17:23:01 +0300

    Create DZEN_gif_400.js

---
+++ platforms/DZEN_gif_400.js
@@ -0,0 +1,58 @@
+
+const { 
+    checkRequestLink,
+    prepareReleaseFolder,
+    getCanvasSize,
+    insertScriptAfterMarker,
+    wrapDiv,
+    compressImages,
+    replaceImagesWithBase64,
+    minifyJSFiles,
+    inlineJavaScript,
+    deleteFiles,
+    archiveFolder,
+    downloadAndReplaceScript,
+    createScreenshotWithTriggerAdaptive
+} = require('../bannerUtils');
+
+module.exports = {
+    name: 'mail_main_gif',
+    process: async (paths, userLink, platformWindow, gifSettings) => {
+        userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
+
+        for (const folderPath of paths) {
+            const releasePath = await prepareReleaseFolder(folderPath);
+            const { width, height } = getCanvasSize(releasePath);
+            console.log(`height ${height}`);
+
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            console.log(`Папка скопирована в ${releasePath}`);
+            console.log(`Используемая ссылка: ${userLink}`);
+
+
+            await insertScriptAfterMarker(releasePath,
+                '<meta charset="UTF-8">',
+                `<meta name="ad.size" content="width=100%,height=${height}">`
+            );
+            
+
+            await downloadAndReplaceScript(releasePath);
+
+            try {
+                await wrapDiv(releasePath, 'banner', `<a href="%link1%" target="_blank">`);
+                console.log('Div успешно обёрнут.');
+            } catch (error) {
+                console.error('Ошибка:', error.message);
+            }
+
+
+            await compressImages(releasePath);
+            // await replaceImagesWithBase64(releasePath);
+            await minifyJSFiles(releasePath);
+            // inlineJavaScript(releasePath);
+            await deleteFiles(releasePath, ['*.fla']);
+            await archiveFolder(releasePath);
+            await createScreenshotWithTriggerAdaptive(folderPath, true, gifSettings, '400');
+        }
+    }
+};
