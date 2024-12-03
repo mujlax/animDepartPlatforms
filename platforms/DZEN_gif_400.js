d8c4c9361bcb04cb66c1ee74463d8bc5022df928
mujlax <42385663+mujlax@users.noreply.github.com>
2024-12-03 17:31:19 +0300

    Update DZEN_gif_400.js

--- platforms/DZEN_gif_400.js
+++ platforms/DZEN_gif_400.js
@@ -16,7 +16,7 @@ const {
 } = require('../bannerUtils');
 
 module.exports = {
-    name: 'mail_main_gif',
+    name: 'dzen_gif_400',
     process: async (paths, userLink, platformWindow, gifSettings) => {
         userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
 
