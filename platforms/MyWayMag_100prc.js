8ca9693ba5f2baa04c3230e65de1062b014b4ced
mujlax <42385663+mujlax@users.noreply.github.com>
2025-01-27 16:46:22 +0300

    Create MyWayMag_100prc.js

---
+++ platforms/MyWayMag_100prc.js
@@ -0,0 +1,56 @@
+
+const { 
+    checkRequestLink,
+    prepareReleaseFolder,
+    getCanvasSize,
+    insertScriptAfterMarker,
+    wrapDiv,
+    compressImages,
+    replaceImagesWithBase64,
+    minifyJSFiles,
+    inlineJavaScript,
+    deleteFiles,
+    archiveFolder,
+    downloadAndReplaceScript
+} = require('./utils/bannerUtils');
+
+module.exports = {
+    name: 'MyWayMag',
+    process: async (paths, userLink, platformWindow) => {
+        userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
+
+        for (const folderPath of paths) {
+            const releasePath = await prepareReleaseFolder(folderPath);
+            const { width, height } = getCanvasSize(releasePath);
+
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            console.log(`Папка скопирована в ${releasePath}`);
+            console.log(`Используемая ссылка: ${userLink}`);
+
+
+            await insertScriptAfterMarker(releasePath,
+                '<meta charset="UTF-8">',
+                `<meta name="ad.size" content="width=100%,height=${height}">`
+            );
+
+            await insertScriptAfterMarker(releasePath,
+                '<!-- write your code here -->',
+                `<script src="/html.js"></script>`
+            );
+
+            try {
+                await wrapDiv(releasePath, 'banner', `<div class="button" onclick="return ar_callLink( {target: '_blank'} );">`);
+                console.log('Div успешно обёрнут.');
+            } catch (error) {
+                console.error('Ошибка:', error.message);
+            }
+
+            await compressImages(releasePath);
+            //await replaceImagesWithBase64(releasePath);
+            await minifyJSFiles(releasePath);
+            //inlineJavaScript(releasePath);
+            await deleteFiles(releasePath, ['*.fla']);
+            await archiveFolder(releasePath);
+        }
+    }
+};
