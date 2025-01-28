b1b31363dedbee6f209252ff7fcaeae255b61b53
mujlax <42385663+mujlax@users.noreply.github.com>
2025-01-28 12:36:47 +0300

    Create OTM.js

---
+++ platforms/OTM.js
@@ -0,0 +1,67 @@
+
+const { 
+    checkRequestLink,
+    prepareReleaseFolder,
+    getCanvasSize,
+    insertScriptAfterMarker,
+    wrapDiv,
+    compressImages,
+    replaceImagesWithBase64,
+    minifyJSFiles,
+    inlineJavaScript,
+    deleteFiles,
+    archiveFolder,
+    downloadAndReplaceScript
+} = require('./utils/bannerUtils');
+
+module.exports = {
+    name: 'OTM',
+    process: async (paths, userLink, platformWindow) => {
+        userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
+
+        for (const folderPath of paths) {
+            const releasePath = await prepareReleaseFolder(folderPath);
+            const { width, height } = getCanvasSize(releasePath);
+
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            console.log(`Папка скопирована в ${releasePath}`);
+            console.log(`Используемая ссылка: ${userLink}`);
+
+            await insertScriptAfterMarker(releasePath,
+                '<meta charset="UTF-8">',
+                `<meta name="ad.size" content="width=${width},height=${height}">`
+            );
+
+          await insertScriptAfterMarker(releasePath,
+                '<script src="https://code.createjs.com/1.0.0/createjs.min.js"></script>',
+                `<script src="https://s0.2mdn.net/ads/studio/cached_libs/createjs_2019.11.15_min.js"></script>`,
+                true
+            );
+
+          await insertScriptAfterMarker(releasePath,
+                '<title>index</title>',
+                `<script type="text/javascript">var clickTag = "";</script>`
+            );
+
+         
+
+         
+
+           
+
+            try {
+                await wrapDiv(releasePath, 'canvas', `<a href="javascript:window.open(window.clickTag)">`);
+                console.log('Div успешно обёрнут.');
+            } catch (error) {
+                console.error('Ошибка:', error.message);
+            }
+
+            await compressImages(releasePath);
+            //await replaceImagesWithBase64(releasePath);
+            await minifyJSFiles(releasePath);
+            //inlineJavaScript(releasePath);
+            await deleteFiles(releasePath, ['*.fla']);
+            await archiveFolder(releasePath);
+        }
+    }
+};
