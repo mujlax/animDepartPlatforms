3684a1e173a51801b5a6a112c1258d9b6ede319c
mujlax <42385663+mujlax@users.noreply.github.com>
2025-01-28 11:15:14 +0300

    Update AdRiver.js

--- platforms/AdRiver.js
+++ platforms/AdRiver.js
@@ -15,9 +15,10 @@ const {
 } = require('./utils/bannerUtils');
 
 module.exports = {
-    name: 'AdRiver(не готов)',
+    name: 'AdRiver(тестировать)',
     process: async (paths, userLink, platformWindow) => {
         userLink = await checkRequestLink(requestLink = true, userLink, platformWindow);
+        userLink2 = await checkRequestLink(requestLink = true, userLink, platformWindow);
 
         for (const folderPath of paths) {
             const releasePath = await prepareReleaseFolder(folderPath);
@@ -34,7 +35,7 @@ module.exports = {
 
             await insertScriptAfterMarker(releasePath,
                 '<body onload="init();" style="margin:0px;">',
-                `<script type="text/javascript" data-placement="" data-pixel="https://ad.adriver.ru/cgi-bin/json.cgi?sid=1&bt=55&ad=797631&pid=4199130&bid=11370455&bn=11370455&exss=&rnd=![rnd]" src="https://content.adriver.ru/banners/0005728/0005728997/0/AV_pixel.js"></script>`,
+                `<script type="text/javascript" data-placement="" data-pixel="${userLink}" src="${userLink2}"></script>`,
             );
 
             // try {
