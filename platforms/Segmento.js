0982c773b8e2ab4c2cce549bc01ce89e4df14ac3
mujlax <42385663+mujlax@users.noreply.github.com>
2025-01-28 16:04:58 +0300

    Create Segmento.js

---
+++ platforms/Segmento.js
@@ -0,0 +1,61 @@
+
+const { 
+    checkRequestLink,
+    prepareReleaseFolder,
+    getCanvasSize,
+    insertScriptAfterMarker,
+    wrapDiv,
+    compressImages,
+    replaceImagesWithBase64,
+    minifyJSFiles,
+    inlineJavaScript,
+    deleteFiles,
+    archiveFolder,
+    downloadAndReplaceScript
+} = require('./utils/bannerUtils');
+
+module.exports = {
+    name: 'Segmento',
+    process: async (paths, userLink, platformWindow) => {
+        userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
+
+        for (const folderPath of paths) {
+            const releasePath = await prepareReleaseFolder(folderPath);
+            const { width, height } = getCanvasSize(releasePath);
+
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            console.log(`Папка скопирована в ${releasePath}`);
+            console.log(`Используемая ссылка: ${userLink}`);
+
+            await insertScriptAfterMarker(releasePath,
+                '<meta charset="UTF-8">',
+                `<meta name="ad.size" content="width=${width},height=${height}">`
+            );
+
+          await insertScriptAfterMarker(releasePath,
+                '<title>index</title>',
+                `<script>var clickTag = "http://www.google.com";</script>`
+            );
+
+         
+
+         
+
+           
+
+            try {
+                await wrapDiv(releasePath, 'animation_container', `<a href="javascript:void(window.open(window.clickTag))">`);
+                console.log('Div успешно обёрнут.');
+            } catch (error) {
+                console.error('Ошибка:', error.message);
+            }
+
+            await compressImages(releasePath);
+            //await replaceImagesWithBase64(releasePath);
+            await minifyJSFiles(releasePath);
+            inlineJavaScript(releasePath);
+            await deleteFiles(releasePath, ['*.fla', 'index.js']);
+            await archiveFolder(releasePath);
+        }
+    }
+};
