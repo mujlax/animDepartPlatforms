d0131ff24e3667365c6061bec6bc3f00e5519d67
mujlax <42385663+mujlax@users.noreply.github.com>
2025-02-10 11:35:22 +0300

    Create yandex_100prc.js

---
+++ platforms/yandex_100prc.js
@@ -0,0 +1,56 @@
+
+const { 
+    checkRequestLink,
+    prepareReleaseFolder,
+    getCanvasSize,
+    insertScriptAfterMarker,
+    wrapDiv,
+    compressImages,
+    replaceImagesWithBase64,
+    minifyJSFiles,
+    inlineJavaScript,
+    deleteFiles,
+    archiveFolder,
+    downloadAndReplaceScript
+} = require('./utils/bannerUtils');
+
+module.exports = {
+    name: 'Yandex_100prc',
+    process: async (paths, userLink, platformWindow) => {
+        userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
+
+        for (const folderPath of paths) {
+            const releasePath = await prepareReleaseFolder(folderPath);
+            const { width, height } = getCanvasSize(releasePath);
+
+            console.log(`Обрабатываем папку: ${folderPath}`);
+            console.log(`Папка скопирована в ${releasePath}`);
+            console.log(`Используемая ссылка: ${userLink}`);
+
+            await insertScriptAfterMarker(releasePath,
+                '<meta charset="UTF-8">',
+                `<meta name="ad.size" content="width=100%,height=${height}">`
+            );
+
+            await insertScriptAfterMarker(releasePath,
+                '</body>',
+                `<script>document.getElementById("click_area").href = yandexHTML5BannerApi.getClickURLNum(1);</script>\n</body>`,
+                true
+            );
+
+            try {
+                await wrapDiv(releasePath, 'banner', `<a id="click_area" href="#" target="_blank">`);
+                console.log('Div успешно обёрнут.');
+            } catch (error) {
+                console.error('Ошибка:', error.message);
+            }
+
+            await compressImages(releasePath);
+            //await replaceImagesWithBase64(releasePath);
+            await minifyJSFiles(releasePath);
+            //inlineJavaScript(releasePath);
+            await deleteFiles(releasePath, ['*.fla']);
+            await archiveFolder(releasePath);
+        }
+    }
+};
