4f2a56371396c48e429f38f6b6af3504983e5eb7
mujlax <42385663+mujlax@users.noreply.github.com>
2025-02-10 12:00:39 +0300

    Update PBD.js

--- platforms/PBD.js
+++ platforms/PBD.js
@@ -18,7 +18,7 @@ const {
 } = require('./utils/bannerUtils');
 
 module.exports = {
-    name: 'PBD',
+    name: 'PBD (тестиоровать)',
     process: async (paths, userLink, platformWindow, gifSettings) => {
         userLink = await checkRequestLink(requestLink = false, userLink, platformWindow);
 
